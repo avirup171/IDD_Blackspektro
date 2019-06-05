85faebd36f14759d9632e04af5d17b557dd6a80e
avirup171 <ami.avirup17@gmail.com>
2019-06-05 15:34:51 +0530

    updated xml_to_csv script

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -12,7 +12,6 @@ def xml_to_csv(path,filepath):
     root = tree.getroot()
     for member in root.findall('object'):
         value = (filepath,
-                root.find('filename').text,
                 int(root.find('size')[0].text),
                 int(root.find('size')[1].text),
                 member[0].text,
@@ -22,23 +21,23 @@ def xml_to_csv(path,filepath):
                 int(member[1][3].text)
                 )
         xml_list.append(value)
-    column_name = ['filepath','filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymax', 'xmax', 'ymin']
     xml_df = pd.DataFrame(xml_list)
     return xml_df
 
 
 def main():
-    column_name = ['filepath','filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymax', 'xmax', 'ymin']
     xml_df = pd.DataFrame(columns=column_name)
     xml_df.to_csv(('images/test_labels.csv'),mode='a', index=None)
-    '''
+    ''''
     with open('train.txt') as t:
         for folder in t:
             filepath="JPEGImages/"+folder.rstrip()+'.jpg'
             folder='Annotations/'+folder.rstrip()+'.xml'
             #print(folder)
             image_path = os.path.join(os.getcwd(), (folder))
-            #print(image_path)
+            print(image_path)
             xml_df = xml_to_csv(image_path,filepath)
             xml_df.to_csv(('images/train_labels.csv'),mode='a', header=False, index=None)
             print('Successfully converted train xml to csv.')
@@ -49,9 +48,9 @@ def main():
             folder='Annotations/'+folder.rstrip()+'.xml'
             #print(folder)
             image_path = os.path.join(os.getcwd(), (folder))
-            #print(image_path)
+            print(image_path)
             xml_df = xml_to_csv(image_path,filepath)
             xml_df.to_csv(('images/test_labels.csv'),mode='a', header=False, index=None)
-            print('Successfully converted test xml to csv.')
+            print('Successfully converted train xml to csv.')
    
 main()
